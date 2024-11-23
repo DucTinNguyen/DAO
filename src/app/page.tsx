d051e4060253d07d5da4a942563fd1034c51a56a
tin.nd <tin.nd@vgamestd.com>
2024-11-23 14:07:39 +0700

    feat: init source

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full flex flex-col items-center bg-[#010D01] text-white font-roBoTo">
+      <div className="w-full flex flex-col items-center justify-start relative">
+        abc
+      </div>
+    </div>
+  );
+}
